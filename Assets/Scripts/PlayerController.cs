e29fe60ebf6f12d25703cf8c753a4385c34e1e10
Zumpo <carlsson889@gmail.com>
2017-06-01 01:16:08 +0200

    Init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    Vector3 velocity;
+
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	public void Move (Vector3 velocity) {
+        this.velocity = velocity;
+	}
+
+    public void FixedUpdate() {
+        rigidBody.MovePosition(rigidBody.position + velocity * Time.fixedDeltaTime);
+    }
+
+    public void LookAt(Vector3 point) {
+        Vector3 heightCorrectedPoint = new Vector3(point.x, transform.position.y, point.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+}
